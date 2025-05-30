b78662834405627f21e64f2b60e4476471752080
Ian <ian@vidales.se>
2025-05-30 22:49:44 +0200

    Add correct base url for GH-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({plugins: [react()], base: '/custom-layout'});
+export default defineConfig({
+  plugins: [react()],
+  base: '/cgp2040-ce-layout-viewer/',
+});
