dbb25aee7ffc37f49a7577649d68e4e72c633b10
Ian <ian@vidales.se>
2025-06-10 21:23:13 +0200

    Change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gp2040-ce-layout-viewer/',
+  base: '/GP2040-CE-layout-viewer/',
 });
